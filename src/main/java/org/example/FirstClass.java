538ed40741df3d86a7c41c3d8baee1495af24fe7
TomasMokry <115876889+TomasMokry@users.noreply.github.com>
2024-01-19 22:33:26 +0100

    First class chaneg

--- src/main/java/org/example/FirstClass.java
+++ src/main/java/org/example/FirstClass.java
@@ -4,4 +4,5 @@ public class FirstClass {
   //my teamleader added some logic here
   // and he added new feature here
   //at the end of the day he made this commit
+  //he is very good so he made this commit here
 }
