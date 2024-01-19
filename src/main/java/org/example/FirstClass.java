7080750b4f0feeced225150a50c020c532aa9e9a
TomasMokry <115876889+TomasMokry@users.noreply.github.com>
2024-01-19 22:36:28 +0100

    Update FirstClass.java

--- src/main/java/org/example/FirstClass.java
+++ src/main/java/org/example/FirstClass.java
@@ -5,4 +5,5 @@ public class FirstClass {
   // and he added new feature here
   //at the end of the day he made this commit
   //he is very good so he made this commit here
+  //here could be change too
 }
