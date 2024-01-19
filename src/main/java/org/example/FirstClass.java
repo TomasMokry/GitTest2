f821594478a8c56273dbcda9e9b31c9cca0cf911
tomas_mokry <tomas.mokry@gmail.com>
2024-01-19 22:37:03 +0100

    my hotfix

--- src/main/java/org/example/FirstClass.java
+++ src/main/java/org/example/FirstClass.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class FirstClass {
   //my teamleader added some logic here
+    //and
+    //here my fix
   // and he added new feature here
   //at the end of the day he made this commit
   //he is very good so he made this commit here
